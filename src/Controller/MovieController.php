7dad9141a45fdf680fe65d91ed290951b186acf5
hectorbertucat <hectorbertucat@gmail.com>
2023-06-01 08:45:54 +0200

    added form to upload image

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\MovieImageFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MovieRepository;
 
@@ -58,6 +62,56 @@ class MovieController extends AbstractController
 
         return $this->render('movie/show.html.twig', [
             'movie' => $movie,
+        ]);
+    }
+
+    #[Route('/movies/{id}/edit', name: 'app_movie_edit')]
+    public function edit($id, MovieRepository $movieRepository, Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $movie = $movieRepository->find($id);
+
+        // if movie not found, redirect to movie index
+        if (!$movie) {
+            return $this->redirectToRoute('app_movie');
+        }
+
+        $form = $this->createForm(MovieImageFormType::class, $movie);
+        $form->handleRequest($request);
+
+        $error = null;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $moviePoster = $form->get('imageFile')->getData();
+
+            if($moviePoster) {
+                $originalFilename = pathinfo($moviePoster->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $movie->getId() .".".$moviePoster->guessExtension();
+
+                try {
+                    $moviePoster->move(
+                        $this->getParameter('movie_poster_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $error = $e->getMessage();
+                }
+
+                $movie->setHasImage(true);
+            }
+
+            $entityManager->persist($movie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_movie_show', [
+                'id' => $movie->getId(),
+                'error' => $error,
+            ]);
+        }
+
+        return $this->render('movie/edit.html.twig', [
+            'addImageForm' => $form->createView(),
+            'movie' => $movie,
+            'error' => $error
         ]);
     }
 }
---
+++ src/Form/MovieImageFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class MovieImageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageFile', FileType::class, [
+                'label' => 'Movie Poster',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Select an image file',
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '3M',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+            ]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
